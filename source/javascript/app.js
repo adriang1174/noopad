24e7555ab9a178521e127108369fc03a8fd5698e
bangkarlsen <jhpe04@gmail.com>
2015-11-21 21:28:56 +0100

    Move layout to neat instead of bootstrap

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -35,6 +35,6 @@ app.config(function(DropboxProvider, noopadConfig) {
         }
 
         
-        $scope.authenticate = login;
+        $scope.login = login;
         $scope.showFile = showFile;
     });
