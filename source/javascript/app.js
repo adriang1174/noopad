d50cc17eab1a04be990715f871cb89c2b239a654
bangkarlsen <jhpe04@gmail.com>
2015-11-22 19:00:20 +0100

    Persist oath token until logout

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -10,18 +10,24 @@ app.config(function(DropboxProvider, noopadConfig) {
         'use strict';
 
         function login() {
-            Dropbox.authenticate().then(function success() {
-                Dropbox.accountInfo().then(function(accountInfo) {
-                    $scope.userDisplayName = accountInfo.name_details.familiar_name;
-                    $toast.show('Logged in as ' + accountInfo.display_name);
-
-                    Dropbox.readdir('/').then(function success(entries) {
-                        $scope.files = entries;
-                    });
-                    
-                });
+            Dropbox.authenticate().then(function success(oauth) {
+                if (oauth.uid) {
+                    localStorage['ngDropbox.oauth'] = angular.toJson(oauth);
+                }
+                getAccount();
             }, function error(reason) {
                 $toast.show('Authentication failed with: ' + reason);
+            });
+        }
+
+        function getAccount() {
+            Dropbox.accountInfo().then(function(accountInfo) {
+                $scope.userDisplayName = accountInfo.name_details.familiar_name;
+                $toast.show('Logged in as ' + accountInfo.display_name);
+
+                Dropbox.readdir('/').then(function success(entries) {
+                    $scope.files = entries;
+                });           
             });
         }
 
@@ -41,6 +47,13 @@ app.config(function(DropboxProvider, noopadConfig) {
             Dropbox.writeFile(filename, body).then(function success() {
                 $toast.show('Saved ' + filename);
             });
+        }
+
+        // on page load try to login
+        if (localStorage['ngDropbox.oauth']) {
+            var oauth = angular.fromJson(localStorage['ngDropbox.oauth']);
+            Dropbox.setCredentials(oauth);
+            getAccount();
         }
         
         $scope.login = login;
