dbe8abebc8bb454010463a4b4e1ab3ebb451dc2a
bangkarlsen <jhpe04@gmail.com>
2016-04-06 21:00:14 +0200

    Use best practise for angular js

--- source/login/login.js
+++ source/login/login.js
@@ -1,26 +1,32 @@
 (function () {
     'use strict';
     
-    var app = angular.module('noopad');
-    app.controller('loginController', function ($location, $toast, $window, Dropbox, noopadKey) {
-        var vm = this;
+    angular.module('noopad')
+        .controller('loginController', function ($location, $log, $toast, $window, Dropbox, noopadKey) {
+            var vm = this;
 
-        vm.login = function () {
-            Dropbox.authenticate().then(function success(oauth) {
-                if (oauth.uid) {
-                    localStorage[noopadKey] = angular.toJson(oauth);
+            function login() {
+                Dropbox.authenticate().then(function success(oauth) {
+                    if (oauth.uid) {
+                        localStorage[noopadKey] = angular.toJson(oauth);
+                        $location.path('/editor');
+                    } else {
+                        $log.error('That\'s weird! Missing oauth token!');
+                    }
+                }, function error(reason) {
+                    $toast.show('Authentication failed with: ' + reason);
+                });
+            }
+
+            // If we already have an auth go directly to editor
+            function setup() {
+                if (localStorage[noopadKey]) {
                     $location.path('/editor');
-                } else {
-                    $window.console.log('Missing oauth token!');
-                }
-            }, function error(reason) {
-                $toast.show('Authentication failed with: ' + reason);
-            });
-        };
+                }            
+            }
 
-        // If we already have an auth go directly to editor
-        if (localStorage[noopadKey]) {
-            $location.path('/editor');
-        }
+            setup();
+
+            vm.login = login;
     });
 })();
